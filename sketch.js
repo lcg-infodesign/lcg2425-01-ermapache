1965f7027d7fdd7f6dc8e94fdecf40daa30c074a
Altanorth <ermanio.malko@mail.polimi.it>
2024-10-24 23:46:37 +0200

    hommage à monet

--- sketch.js
+++ sketch.js
@@ -1,17 +1,35 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
-}
+  createCanvas(1280, 720);
+  background(255);
+  noStroke();
 
-function draw() {
-  // put drawing code here
+  let cols=9;
+  let rows=45;
+
+  let cellWidth= width/cols;
+  let cellHeight=height/rows;
+
+  let rectWidth=cellWidth-45; 
+  let rectHeight=cellHeight-35; 
+
+  let baseColor=color(200, 184, 148, 150);  //beige
+  let userColor=color(90, 182, 243, 150);   //blu
+
+  for (let j=0; j<rows; j++) {
+    for (let i=0; i<cols; i++) {
+      let x=i*cellWidth;
+      let y=j*cellHeight;
+
+      let offsetX=random(-cellWidth*0.1, cellWidth*0.1);
+      let offsetY=random(-cellHeight*0.02, cellHeight*0.02);
+      let useUserColor = random(1) < 0.1;   //spawnrate 10% blu
+
+      if (useUserColor) {
+        fill(userColor);
+      } else {
+        fill(baseColor);
+      }
+      rect(x + offsetX, y + offsetY, rectWidth, rectHeight);
+    }
+  }
 }
